15aaf4f0f0bdb4a09a142aae3fdc7dc80560b878
BetFab <fabre.betty@gmail.com>
2016-02-18 16:43:14 +0100

    cablageInverse fonction qui renvoie la lettre décodée, données des réflecteurs

--- codes/chiffrage.c
+++ codes/chiffrage.c
@@ -1,12 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fonctions.h"
 
 int main()
 {
+
+   /* CABLAGE DES ROTORS */
    char * rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
    char * rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
    char * rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
    char * rotor4 = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
    char * rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK";
+
+   /* CABLAGE DES REFLECTEURS */
+   char * reflecteurA = "EJMZALYXVBWFCRQUONTSPIKHGD";
+   char * reflecteurB = "YRUHQSLDPXNGOKMIEBFZCWVJAT" ;
+
+   char * lettre = "J";
+
+
+
+   return 0;
 }
--- codes/fonctions.c
+++ codes/fonctions.c
@@ -1,3 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "fonctions.h"
+
+
+int cablageInverse (char * cablage, char lettre)
+{
+  int i ;
+  int trouve = 0 ; /* booleen qui arrete la recherche si lettre trouvee */
+  while(!trouve)
+  {
+    if(cablage[i] == lettre)
+    {
+      trouve++;
+    }
+    i++; /* retourne l'indice +1 si trouve et incremente sinon */
+  }
+
+  return(i);
+}
