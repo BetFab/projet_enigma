5a299e09dcc5d38b79fa76a5261a5d1ef93fc03a
BetFab <fabre.betty@gmail.com>
2016-02-18 16:18:10 +0100

    rotors

--- codes/chiffrage.c
+++ codes/chiffrage.c
@@ -1,3 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "fonctions.h"
+
+int main()
+{
+   rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+   rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+   rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+   rotor4 = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+   rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK";
+}
