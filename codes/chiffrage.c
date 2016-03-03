a0463fa7312851fd3c04e681dc37e859514dc7c2
BetFab <fabre.betty@gmail.com>
2016-03-03 09:42:28 +0100

    fonction de rotation des rotors + décalage (non testée)

--- codes/chiffrage.c
+++ codes/chiffrage.c
@@ -26,16 +26,49 @@ int main()
    char         lettrechiffree ;
 
    int       * choixRotors = {1 , 2 , 3};
+   char      * positionIntialeRotors{'O' , 'Y' , 'H'}; /* clé du jour */
+   int       * indicePositionInitialeRotors{12, 21, 3}; /* fonction à implementer /!\ */
 
    /* On stock l'indice de départ des alphabets dans un tableau d'entier, cette indice
       est initialisée en paramètre par la position de départ, puis modifié par la
       fonction "qui tourne" les rotors */
 
-   char        positionInitialeRotors[3] = {'F','U','L'};
-   int         indicePositionInitialeRotors[3] = {-1, -1, -1};
+
+  /* /!\ /!\ SEULE UNE ENCOCHE EST UTILE ? PREMIERE ? */
+
+   char        positionEncocheRotors[3] = {'F','U','L'};
+   int         indicePositionEncocheRotors[3] = {-1, -1, -1};
    int         positionRotors[3] ;
 
    int         i = 0;
+   int         nbFrappes = 0;
+
+   int        decalageRotor1 = indicePositionInitialeRotors[0];
+   int        decalageRotor2 = indicePositionInitialeRotors[1];
+   int        decalageRotor3 = indicePositionInitialeRotors[3];
+
+   /* On va essayer d'encrypter TES */
+   char l1 = 'T';
+   char l2 = 'E';
+   char l3 = 'S';
+
+   nbFrappes++ ;
+   l1 = lettreChiffree(rotor1, l1, decalageRotor1);
+   l1 = lettreChiffree(rotor2, l1, decalageRotor2);
+   l1 = lettreChiffree(rotor3, l1, decalageRotor3);
+   rotationRotor( nbFrappes, &decalageRotor1, &decalageRotor2, &decalageRotor3, indicePositionEncocheRotors ,indicePositionInitialeRotors)
+
+   nbFrappes++;
+   l2 = lettreChiffree(rotor1, l2, decalageRotor1);
+   l2 = lettreChiffree(rotor2, l2, decalageRotor2);
+   l2 = lettreChiffree(rotor3, l2, decalageRotor3);
+   rotationRotor( nbFrappes, &decalageRotor1, &decalageRotor2, &decalageRotor3, indicePositionEncocheRotors ,indicePositionInitialeRotors)
+
+   nbFrappes++;
+   l3 = lettreChiffree(rotor1, l3, decalageRotor1);
+   l3 = lettreChiffree(rotor2, l3, decalageRotor2);
+   l3 = lettreChiffree(rotor3, l3, decalageRotor3);
+   rotationRotor( nbFrappes, &decalageRotor1, &decalageRotor2, &decalageRotor3, indicePositionEncocheRotors ,indicePositionInitialeRotors)
 
    /* ETAPE 1 convertir les lettres en indices de leur position dans l'alphabet */
    /* ETAPE 2 à chaque fois que le rotor passe sur l'indice initiale
@@ -44,7 +77,9 @@ int main()
               -> à chaque 26 tour du rotor1, le rotor 2 a fait 1 tour
               -> donc le rotor 3 tourne */
 
-  indexPositionDepart(rotor1, rotor2, rotor3, positionInitialeRotors, indicePositionInitialeRotors);
+  indexPositionDepart(rotor1, rotor2, rotor3, positionEncocheRotors, indicePositionEncocheRotors);
+
+
 
   /*test*/
   /*
--- codes/fonctions.c
+++ codes/fonctions.c
@@ -21,14 +21,30 @@ int cablageInverse (char * cablage, char lettre) /* testée ok */
   return(i+64);
 }
 
-int lettreChiffree (char * cablage, char lettre) /*testée ok */
+int lettreChiffree (char * cablage, char lettre, int decalage)/* non testée avec décalage */
 {
-  return(cablage[lettre-65]) ;
+  return(cablage[ ((lettre-65) + decalage) % 26]) ;
 }
 
 void indexPositionDepart( char * rotor1, char * rotor2, char * rotor3 , char * lettresDepart, int * indices)
 {
+    /* testée ok mais potentiellement inutile */
      indices[0] = cablageInverse(rotor1, lettresDepart[0]) - 65;
      indices[1] = cablageInverse(rotor2, lettresDepart[1]) - 65;
      indices[2] = cablageInverse(rotor3, lettresDepart[2]) - 65;
+}
+
+void rotationRotor( int compteurFrappes, int * decR1, int * decR2, int * decR3, int * indiceEncoche int * positionInitRotors)
+{
+    ((*decR1)++) % 26; /* le rotor1 tourne 1 fois */
+    if( ((positionInitRotors[0] - compteurFrappes)%26) ==  indiceEncoche[0] )
+    {
+        /* le rotor 2 tourne 1 fois */
+        ((*decR2)++) % 26;
+        if( ((positionInitRotors[1] - compteurFrappes)%26) ==  indiceEncoche[1] )
+        {
+          /* le rotor 3 tourne 1 fois */
+          ((*decR3)++) % 26;
+        }
+    }
 }
