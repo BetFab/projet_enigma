3ad747749f2d5c6dff1deb3da290b9f61c784feb
chtahri <tahri.chyrine@gmail.com>
2016-05-06 17:18:03 +0200

    blabla
    blabla

--- codes/correcteur.c
+++ codes/correcteur.c
@@ -2,38 +2,39 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int accent(char c,char tab[11]) /*retourne 1 si le caractère n'appartient pas à la chaine, 0 sinon*/
+int indice_occur(char c,char tab[11]) /*on s'y appuiera pour déterminer si une lettre est accentuée ou pas*/
 {
-    int res=1; int i=0;
-    while((i<11) && (res!=0))
+    char cour=tab[0]; int i=0; int indice=-1;
+    while((i<11)&&(indice<0))
     {
-        if (tab[i]==c) res=0;
+        if (cour==c) indice=i;
         i++;
     }
-    return res;
+    return indice;
 }
 void correction()
 {
     FILE* fichier=NULL;
     int caractere=0;
     char tab={'à','ä','é','è','ë','ê','î','ï','ù','ü','û'};
+    char tab_c={'a','a','e','e','e','e','i','i','u','u','u'};
     fichier=fopen("message.txt","r+");
     if (fichier!=NULL){
         do
         {
             caractere=fgetc(fichier); /*On se place sur le caractère courant*/
-            if (!accent(caractere,tab)) /*ce n'est pas une lettre accentuée*/
+            if (indice_occur(caractere,tab)<0) /*ce n'est pas une lettre accentuée*/
             {
-                if isupper(caractere)/*la lettre est majuscule*/
+                if (isupper(caractere))/*la lettre est majuscule*/
                 {
                     caractere=tolower(caractere);/*on la transforme en miniscule*/
                 }
             }
             else
             {
-                    /*Fonction à définir pour les accents*/
+                caractere=tab_c[indice_occur(caractere,tab)];/*On matche la lettre avec celle non accentuée*/
             }
 
-    }
+        } while (fgetc(fichier)!=NULL); /*ou EOF ? */
 
 }
